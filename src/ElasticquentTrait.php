370875e6ac9c174d49865a83e1d4a288d945e593
Rohan <me@rohandey.com>
2015-12-12 17:57:48 +0530

    Update ElasticquentTrait.php

--- src/ElasticquentTrait.php
+++ src/ElasticquentTrait.php
@@ -251,7 +251,7 @@ trait ElasticquentTrait
         return $all->reindex();
     }
 
-    /**
+ /**
      * Search By Query
      *
      * Search with a query array
@@ -263,23 +263,28 @@ trait ElasticquentTrait
      * @param   int $offset
      * @return  ResultCollection
      */
-    public static function searchByQuery($query = null, $limit = null, $offset = null)
+    public static function searchByQuery($query = null, $aggregations = null, $sourceFields = null, $limit = null, $offset = null, $sort = null)
     {
         $instance = new static;
-
-        $params = $instance->getBasicEsParams('read', true, true, $limit, $offset);
-
-        if ($query) {
-            $params['body'] = $query;
-        } else {
-            $params['body']['query'] = ['match_all' => []];
+        $params = $instance->getBasicEsParams(true, true, true, $limit, $offset);
+        if ($sourceFields) {
+            $params['body']['_source']['include'] = $sourceFields;
         }
-
+        if ($query) {
+            $params['body']['query'] = $query;
+        }
+        if ($aggregations) {
+            $params['body']['aggs'] = $aggregations;
+        }
+        
+        if ($sort) {
+            $params['body']['sort'] = $sort;
+        }
         $result = $instance->getElasticSearchClient()->search($params);
-        $result['query'] = json_encode($query);
-        return collect($result);
+        return new ResultCollection($result, $instance = new static);
     }
-
+    
+    
     /**
      * Search
      *
